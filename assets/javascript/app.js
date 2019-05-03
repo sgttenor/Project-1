fa1f042f2dcdfd6e856ce8c599ac24153c61391a
Marco Teti <sgttenore.gmail.com>
2019-05-03 14:41:52 -0700

    updated styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,3 @@
-var topics = [];
-
-
-function displayInfo() {
-
-var queryURL ="https://api.spotify.com/v1" + topic + "/browse/categories/{1856f37d7b6c45e99c921406864f622f}";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).done(function (response) {
-
-    
-  });
-
-}
 
 
 
