e45aebec15df3097d8c596b8cb8af8abaa07051b
Carlos Quinto <carlquin77@gmail.com>
2019-05-03 13:16:17 -0700

    added deezer display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,39 +17,12 @@ $(document).ready(function () {
         jqXHR.setRequestHeader("X-RapidAPI-Host", "deezerdevs-deezer.p.rapidapi.com");
         jqXHR.setRequestHeader("X-RapidAPI-Key", "bbc38e84eamsh62eb5256b273599p1a8523jsn69457858fec8");
       },
-      method: "GET",
-    }).then(function (response) {
+      method: "GET"
+    }).then(function(response) {
       console.log(response);
-      console.log(response.data[0].link)
-      
+      console.log(response.data[0].link);
     });
   }
 
 });
-
-function displayInfo() {
-  var topic = $("this").attr("data-name");
-  var queryURL = "https://api.spotify.com/v1" + topic + "/browse/categories/{1856f37d7b6c45e99c921406864f622f}";
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function (response) {
-
-    //empty gif div so new selection appends to emtpy div - do not want previous searches listed
-
-    $("#forest").empty();
-
-
-
-    //user for loop to grab the rating information and appropriate gif for button clicked into its own div to keep information together
-
-
-
-    //on click of gif still image, gif will play. When clicked again, gif will pause.
-
-
-  });
-
-}
 
